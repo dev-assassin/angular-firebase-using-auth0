11f4042959ec9dcd1e0cb1744bb157ee882b5f7a
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-20 14:36:47 +0800

    create dog and dogs components

---
+++ angular-firebase/src/app/dogs/dogs.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DogsComponent } from './dogs/dogs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DogsComponent]
+})
+export class DogsModule { }
