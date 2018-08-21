8d13ba96bc02f7fe3b0cd77ec8f7ffa78c4d1b35
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-21 22:54:16 +0800

    update loading component

--- angular-firebase/src/app/core/loading.component.ts
+++ angular-firebase/src/app/core/loading.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  templateUrl: `
+    <div [ngClass]="{'inline': inline, 'text-center': !inline, 'py-2': !inline}">
+      <img src="/assets/images/loading.svg">
+    </div>
+  `,
+  styleUrls: [`
+    .inline {
+      display: inline-block;
+    }
+
+    img {
+      height: 80px;
+      width: 80px;
+    }
+
+    .inline img {
+      height: 24px;
+      width: 24px;
+    }
+  `]
 })
-export class LoadingComponent implements OnInit {
+export class LoadingComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() inline: boolean;
 
 }
