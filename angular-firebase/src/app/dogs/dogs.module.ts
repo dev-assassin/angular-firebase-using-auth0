35dfc1d1745496b3ff9dff626b15513087b26d74
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-21 16:42:26 +0800

    update dog/dogs module

--- angular-firebase/src/app/dogs/dogs.module.ts
+++ angular-firebase/src/app/dogs/dogs.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule} from '@angular/router';
+import { CoreModule } from '../core/core.module';
+import { CommentsModule } from '../comments/comments.module';
 import { DogsComponent } from './dogs/dogs.component';
 
+const DOGS_ROUTES: Routes = [
+  {
+    path: '',
+    component: DogsComponent
+  }
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule,
+    RouterModule.forChild(DOGS_ROUTES),
+    CommentsModule
   ],
   declarations: [DogsComponent]
 })
