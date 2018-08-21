882e06a5c5492f5b39d50ddc90732b69c88a9c33
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-21 10:17:12 +0800

    update auth/comments/core module files

--- angular-firebase/src/app/comments/comments.module.ts
+++ angular-firebase/src/app/comments/comments.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from '../core/core.module';
+import { environment } from '../../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule  } from 'angularfire2/firestore';
 import { CommentsComponent } from './comments/comments.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  declarations: [CommentsComponent, CommentFormComponent],
+  declarations: [
+    CommentsComponent,
+    CommentFormComponent
+  ],
   exports: [CommentsComponent]
 })
 export class CommentsModule { }
