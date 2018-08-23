ca86250e6580f49704cfc6bd29f8f49a947dd65a
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-23 17:27:57 +0800

    update api service

--- angular-firebase/src/app/core/api.service.ts
+++ angular-firebase/src/app/core/api.service.ts
@@ -1,7 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { AuthService } from './../auth/auth.service';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import 'rxjs/add/observable/throw';
+import { Dog } from './../core/dog';
+import { DogDetail } from './../core/dog-detail';
 
 @Injectable()
 export class ApiService {
+  private _API = `${environment.apiRoot}api`;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService) { }
+
+  getDogs$(): Observable<Dog[]> {
+    return this.http
+      .get(`${this._API}/dogs`)
+      .pipe(
+        catchError((err, caught) => this._onError(err, caught))
+      );
+  }
+
+  getDogByRank$(rank: number): Observable<DogDetail> {
+    return this.http
+      .get(`${this._API}/dog/${rank}`, {
+        headers: new HttpHeaders().set('Authorization', `Bearer ${this.auth.accessToken}`)
+      })
+      .pipe(
+        catchError((err, caught) => this._onError(err, caught))
+      );
+  }
+
+  private _onError(err, caught) {
+    let errorMsg = 'Error: Unable to complete request.';
+    if (err instanceof HttpErrorResponse) {
+      errorMsg = err.message;
+      if (err.status === 401 || errorMsg.indexOf('No JWT') > -1 || errorMsg.indexOf('Unauthorized') > -1) {
+        this.auth.login();
+      }
+    }
+    return Observable.throw(errorMsg);
+  }
+
 }
