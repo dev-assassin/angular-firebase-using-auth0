93d74d30f4ba3fd36da7713acd83761f7cdee659
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-24 22:56:46 +0800

    add dog component ts file

--- angular-firebase/src/app/dog/dog/dog.component.ts
+++ angular-firebase/src/app/dog/dog/dog.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+import { DogDetail } from '../../core/dog-detail';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../core/api.service';
+import { tap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dog',
   templateUrl: './dog.component.html',
-  styles: []
+  styles: [`
+    .dog-photo {
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+      background-size: cover;
+      min-height: 250px;
+      width: 100%;
+    }
+  `]
 })
-export class DogComponent implements OnInit {
+export class DogComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  paramSub: Subscription;
+  dog$: Observable<DogDetail>;
+  loading = true;
+  error: boolean;
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private title: Title
+  ) { }
 
   ngOnInit() {
+    this.paramSub = this.route.params
+      .subscribe(
+        params => {
+          this.dog$ = this.api.getDogByRank$(params.rank).pipe(
+            tap(val => this._onNext(val)),
+            catchError((err, caught) => this._onError(err, caught))
+          );
+        }
+      );
+  }
+
+  private _onNext(val: DogDetail) {
+    this.loading = false;
+  }
+
+  private _onError(err, caught): Observable<any> {
+    this.loading = false;
+    this.error = true;
+    return Observable.throw('An error occurred fetching detail data for this dog.');
+  }
+
+  getPageTitle(dog: DogDetail): string {
+    const pageTitle = `#${dog.rank}: ${dog.breed}`;
+    this.title.setTitle(pageTitle);
+    return pageTitle;
+  }
+
+  getImgStyle(url: string) {
+    return `url(${url})`;
+  }
+
+  ngOnDestroy() {
+    this.paramSub.unsubscribe();
   }
 
 }
