ae0e711d37a21461dc82ccbc47b5fe3b884d6cb3
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-28 20:44:36 +0800

    add comments component

--- angular-firebase/src/app/comments/comments/comments.component.ts
+++ angular-firebase/src/app/comments/comments/comments.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnInit {
+export class CommentsComponent {
 
-  constructor() { }
+  private _commentsCollection: AngularFirestoreCollection<Comment>;
+  comments$: Observable<Comment[]>;
+  loading = true;
+  error: boolean;
 
-  ngOnInit() {
+  constructor(
+    private afs: AngularFirestore,
+    public auth: AuthService
+  ) {
+    // Get latest 15 comments from Firestore, ordered by timestamp
+    this._commentsCollection = afs.collection<Comment>(
+      'comments',
+      ref => ref.orderBy('timestamp').limit(15)
+    );
   }
 
 }
