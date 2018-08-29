f033136284bc139e8e24e4e5f32e1888cc294597
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-29 10:15:50 +0800

    update comment-form component

--- angular-firebase/src/app/comments/comments/comment-form/comment-form.component.ts
+++ angular-firebase/src/app/comments/comments/comment-form/comment-form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Comment } from './../../comment';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-comment-form',
-  templateUrl: './comment-form.component.html',
-  styles: []
+  templateUrl: './comment-form.component.html'
 })
 export class CommentFormComponent implements OnInit {
+  @Output() postComment = new EventEmitter<Comment>();
+  commentForm: Comment;
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this._newComment();
+  }
+
+  private _newComment() {
+    this.commentForm = new Comment(
+      this.auth.userProfile.name,
+      this.auth.userProfile.sub,
+      this.auth.userProfile.picture,
+      '',
+      null);
+  }
+
+  onSubmit() {
+    this.commentForm.timestamp = new Date().getTime();
+    console.log(this.commentForm.timestamp, new Date().getTime(), '----');
+    this.postComment.emit(this.commentForm);
+    this._newComment();
+    console.log(this.commentForm, '*commentForm*');
   }
 
 }
